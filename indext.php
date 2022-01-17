4a4d662162308af6fb87cbc8cbd02d5f3da16229
diki <dikiweb317.com>
2022-01-17 17:58:27 +0700

    menambakan foreach

--- indext.php
+++ indext.php
@@ -1,28 +1,9 @@
 <?php
 
-
-
-// $nama = ['elfa', 'elma', 'amanda'];
-
-// var_dump($nama);
-// $nama [] = "gisel";
-// $nama [] = "eca";
-
-// echo "<br>";
-
-// var_dump($nama);
-
-
-// pengulangan pada array
-// for / foreach
-
-// $nama = ['elma', "elfa", 'siti', 'nessa'];
-
-// for($i = 0 ; $i < 3; $i++){
-//     echo $nama[$i];
-// }
+$angka = [1,2,3,4,5,6,7,8];
 
 ?>
+
 
 <!DOCTYPE html>
 <html lang="en">
@@ -31,31 +12,33 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <style>
+        .kotak {
+            width: 50px;
+            height: 50px;
+            background-color: salmon;
+            text-align: center;
+            line-height: 50px;
+            margin: 5px;
+            float: left;
+
+        }
+
+        .clear {
+            clear: both;
+        }
+    </style>
 </head>
-<style>
-
-div {
-    height: 50px;
-    width: 50px;
-    background-color: salmon;
-    line-height: 50px;
-    text-align: center;
-    float: left;
-    margin-left: 5px;
-}
-
-
-</style>
 <body>
-   
-<?php
-$angka =[1,2,3,4,5,6,7]
 
-?>
-for($i  = 0; $i < 6; $i++) { ?>
+<?php for ($i = 0; $i < count($angka); $i++) { ?>
+<div class="kotak"> <?php  echo $angka[$i] ?> </div>
+<?php  } ?>
 
-<div>3</div>
+<div class="clear"></div>
 
+<?php   foreach ($angka as $a ){ ?>
+<div class="kotak"> <?php echo $a  ?> </div>
 <?php } ?>
 
 </body>
