bbbfd379b1e804a0036755f0d576c700836e864c
diki <dikiweb317.com>
2022-01-17 16:16:39 +0700

    mencoba for untuk array

--- indext.php
+++ indext.php
@@ -2,9 +2,61 @@
 
 
 
-$nama = ['elfa', 'elma', 'amanda'];
+// $nama = ['elfa', 'elma', 'amanda'];
 
-echo $nama[1];
+// var_dump($nama);
+// $nama [] = "gisel";
+// $nama [] = "eca";
 
+// echo "<br>";
+
+// var_dump($nama);
+
+
+// pengulangan pada array
+// for / foreach
+
+// $nama = ['elma', "elfa", 'siti', 'nessa'];
+
+// for($i = 0 ; $i < 3; $i++){
+//     echo $nama[$i];
+// }
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<style>
+
+div {
+    height: 50px;
+    width: 50px;
+    background-color: salmon;
+    line-height: 50px;
+    text-align: center;
+    float: left;
+    margin-left: 5px;
+}
+
+
+</style>
+<body>
+   
+<?php
+$angka =[1,2,3,4,5,6,7]
+
+?>
+for($i  = 0; $i < 6; $i++) { ?>
+
+<div>3</div>
+
+<?php } ?>
+
+</body>
+</html>
